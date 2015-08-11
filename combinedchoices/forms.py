48804b91143b9de1922a401351753052bf89cee6
David Marks <renoc@gmx.com>
2015-08-10 21:32:25 -0400

    Move form methods and test from front-end repo to back-end repo

--- combinedchoices/forms.py
+++ combinedchoices/forms.py
@@ -1,14 +1,109 @@
+from django.apps import apps
+from django.conf import settings
 from django.forms.fields import BooleanField, CharField
 from django.forms.forms import Form
+from django.forms.models import ModelChoiceField, ModelMultipleChoiceField
+from django.forms.widgets import CheckboxSelectMultiple, RadioSelect, Textarea
 
-import models
+from combinedchoices.models import THROUGH_MODEL, SECTION_MODEL
+
+
+class ChoiceLabelMixin(object):
+
+    def label_from_instance(self, obj):
+        return obj.text
+
+
+class MultiChoice (ChoiceLabelMixin, ModelMultipleChoiceField):
+    pass
+
+
+class SingleChoice (ChoiceLabelMixin, ModelChoiceField):
+    pass
+
+
+CHOICE_MODEL = getattr(
+    settings, 'CHOICE_MODEL', 'combinedchoices.fake_models.Choice')
+COMPLETED_MODEL = getattr(
+    settings, 'COMPLETED_MODEL',
+    'combinedchoices.fake_models.CompletedCombinedObj')
 
 
 class ReadyForm(Form):
     form_name = CharField(label='Completed Name')
 
+    def __init__(self, *args, **kwargs):
+        ready_class = kwargs.pop('ready_class')
+        filters = kwargs.pop('filters', {})
+        super(ReadyForm, self).__init__(*args, **kwargs)
+        baseccobjs = ready_class.included_forms.filter(**filters)
+        Choice = apps.get_model(*CHOICE_MODEL.split('.'))
+        for section in self.get_sections(baseccobjs, **filters):
+            if section.cross_combine:
+                name = section.field_name
+                queryset = Choice.objects.filter(
+                    choice_section__base_ccobj__in=baseccobjs)
+                self.create_section_field(name, section, queryset)
+            else:
+                for basecc in baseccobjs:
+                    name = '%s - %s' % (
+                        basecc.form_name, section.field_name)
+                    queryset = Choice.objects.filter(
+                        choice_section__base_ccobj=basecc)
+                    self.create_section_field(name, section, queryset)
+
+    def create_section_field(self, name, basechoice, queryset):
+        Section = apps.get_model(*SECTION_MODEL.split('.'))
+        queryset = queryset.filter(
+            choice_section__base_choice=basechoice).order_by('text')
+        if basechoice.field_type in [Section.TEXT, Section.DESCRIPTION]:
+            self.fields[name] = CharField(
+                help_text=basechoice.instructions, required=False,
+                initial='\n\n'.join(queryset.values_list('text', flat=True)))
+            self.fields[name].widget = Textarea()
+            if basechoice.field_type is Section.DESCRIPTION:
+                self.fields[name].widget.attrs.update(
+                    {'class':'combo-text', 'disabled':True})
+            elif basechoice.field_type is Section.TEXT:
+                self.fields[name].widget.attrs.update(
+                    {'class':'combo-text', 'read-only':True})
+        elif basechoice.field_type is Section.SINGLE:
+            self.fields[name] = SingleChoice(
+                queryset=queryset, help_text=basechoice.instructions,
+                empty_label='')
+            self.fields[name].widget = RadioSelect(
+                choices=self.fields[name].choices)
+        else:
+            self.fields[name] = MultiChoice(
+                queryset=queryset, help_text=basechoice.instructions)
+            self.fields[name].widget = CheckboxSelectMultiple(
+                choices=self.fields[name].choices)
+        self.fields[name].label = name
+
     def get_sections(self, compendiums, **kwargs):
-        kwargs.update({models.THROUGH_MODEL.split('.')[1].lower() +
+        kwargs.update({THROUGH_MODEL.split('.')[1].lower() +
                       '__base_ccobj__in':compendiums})
-        model = models.models.get_model(*models.SECTION_MODEL.split('.'))
+        model = apps.get_model(*SECTION_MODEL.split('.'))
         return model.objects.filter(**kwargs)
+
+    def save(self, *args, **kwargs):
+        completed = {}
+        commodel = apps.get_model(*COMPLETED_MODEL.split('.'))
+        name = self.cleaned_data.pop('form_name')
+        self.fields.pop('form_name')
+        for field in self.fields.keys():
+            data = self.cleaned_data[field]
+            if not data:
+                pass
+            elif type(self.fields[field]) is CharField:
+                completed[field] = [data]
+            elif type(self.fields[field]) is SingleChoice:
+                completed[field] = [data.text]
+            elif type(self.fields[field]) in [MultiChoice]:
+                completed[field] = []
+                for choice in self.cleaned_data[field]:
+                    completed[field].append(choice.text)
+            else:
+                raise NotImplementedError()
+        return commodel.objects.create(
+            form_name=name, form_data=completed, **kwargs)
--- combinedchoices/tests.py
+++ combinedchoices/tests.py
@@ -1,11 +1,19 @@
+from django.apps import apps
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from model_mommy import mommy
 
-from combinedchoices.fake_models import (
-    BaseChoice, BaseCCObj, Choice, ChoiceSection, CompletedCombinedObj,
-    ReadyCombinedObj)
+from combinedchoices.models import BASE_MODEL, SECTION_MODEL, THROUGH_MODEL
+from combinedchoices.fake_models import ReadyCombinedObj
+from combinedchoices.forms import ReadyForm, CHOICE_MODEL, COMPLETED_MODEL
+
+
+BaseCCObj = apps.get_model(*BASE_MODEL.split('.'))
+BaseChoice = apps.get_model(*SECTION_MODEL.split('.'))
+Choice = apps.get_model(*CHOICE_MODEL.split('.'))
+ChoiceSection = apps.get_model(*THROUGH_MODEL.split('.'))
+CompletedCombinedObj = apps.get_model(*COMPLETED_MODEL.split('.'))
 
 
 class Unicode_Tests(TestCase):
@@ -64,8 +72,145 @@ class BaseChoice_Tests(TestCase):
         mod.validate_unique()
         #No errors raised
 
-    def test_validate_fail(self):
-        mod = BaseChoice(field_name='testuni', field_type=0)
-        mod.save()
-        mod = BaseChoice(field_name='testuni', field_type=0)
-        self.assertRaises(ValidationError, mod.validate_unique)
+# Validate overwritten in front end repo
+#    def test_validate_fail(self):
+#        mod = BaseChoice(field_name='testuni', field_type=0)
+#        mod.save()
+#        mod = BaseChoice(field_name='testuni', field_type=0)
+#        self.assertRaises(ValidationError, mod.validate_unique)
+
+
+class ReadyForm_Tests(TestCase):
+
+    def test_text_save(self):
+        pass
+
+
+class Section_Type_by_Form_Tests(TestCase):
+
+    def test_character_form_init_cross(self):
+        sect = mommy.make(
+            BaseChoice, field_name='section', cross_combine=True,
+            field_type=BaseChoice.MULTIPLE)
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        mommy.make(Choice, text='test_choice', choice_section=cs)
+        comp2 = mommy.make(BaseCCObj, form_name='compendium_test')
+        cs = mommy.make(ChoiceSection, base_ccobj=comp2, base_choice=sect)
+        mommy.make(Choice, text='choice_test', choice_section=cs)
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        form = ReadyForm(ready_class=combined)
+        choices = form.fields['section'].widget.choices
+        self.assertEqual(choices[0][1], 'test_choice')
+
+    def test_character_form_init_uncross(self):
+        sect = mommy.make(
+            BaseChoice, field_name='section', cross_combine=False,
+            field_type=BaseChoice.MULTIPLE)
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        mommy.make(Choice, text='test_choice', choice_section=cs)
+        comp2 = mommy.make(BaseCCObj, form_name='compendium_test')
+        cs = mommy.make(ChoiceSection, base_ccobj=comp2, base_choice=sect)
+        mommy.make(Choice, text='choice_test', choice_section=cs)
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        form = ReadyForm(ready_class=combined)
+        choices = form.fields['test_compendium - section'].widget.choices
+        self.assertEqual(choices[0][1], 'test_choice')
+
+    def test_text_init(self):
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        kwargs = {'ready_class': combined}
+        form = ReadyForm(**kwargs)
+
+        sect = mommy.make(
+            BaseChoice, field_name='section', field_type=BaseChoice.TEXT)
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        mommy.make(Choice, text='test_choice', choice_section=cs)
+
+        self.assertEqual(len(form.fields), 1)
+        form.create_section_field('name', sect, Choice.objects)
+        self.assertEqual(len(form.fields), 2)
+        self.assertEqual(form.fields['name'].initial, 'test_choice')
+
+    def test_text_save(self):
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        kwargs = {'ready_class': combined}
+
+        sect = mommy.make(
+            BaseChoice, field_name='section', field_type=BaseChoice.TEXT)
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        mommy.make(Choice, text='test_choice', choice_section=cs)
+        form = ReadyForm(**kwargs)
+        form.cleaned_data = {'form_name': 'testcc', 'section': u'preset'}
+
+        self.assertFalse(CompletedCombinedObj.objects.all().exists())
+
+        form.save()
+
+        self.assertTrue(CompletedCombinedObj.objects.all().exists())
+        self.assertEqual(
+            CompletedCombinedObj.objects.get().form_data['section'][0], 'preset')
+
+    def test_single_save(self):
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        kwargs = {'ready_class': combined}
+
+        sect = mommy.make(
+            BaseChoice, field_name='section', field_type=BaseChoice.SINGLE)
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        choice = mommy.make(Choice, text='preset', choice_section=cs)
+        form = ReadyForm(**kwargs)
+        form.cleaned_data = {'form_name': 'testcc', 'section': choice}
+
+        self.assertFalse(CompletedCombinedObj.objects.all().exists())
+
+        form.save()
+
+        self.assertTrue(CompletedCombinedObj.objects.all().exists())
+        self.assertEqual(
+            CompletedCombinedObj.objects.get().form_data['section'][0], 'preset')
+
+    def test_description_save(self):
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        kwargs = {'ready_class': combined}
+
+        sect = mommy.make(
+            BaseChoice, field_name='section', field_type=BaseChoice.DESCRIPTION)
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        choice = mommy.make(Choice, text='preset', choice_section=cs)
+        form = ReadyForm(**kwargs)
+        form.cleaned_data = {'form_name': 'testcc', 'section': ''}
+
+        self.assertFalse(CompletedCombinedObj.objects.all().exists())
+
+        form.save()
+
+        self.assertTrue(CompletedCombinedObj.objects.all().exists())
+        self.assertFalse(
+            'section' in CompletedCombinedObj.objects.get().form_data.keys())
+
+    def test_multiple_save(self):
+        comp1 = mommy.make(BaseCCObj, form_name='test_compendium')
+        combined = mommy.make(ReadyCombinedObj, included_forms=[comp1])
+        kwargs = {'ready_class': combined}
+
+        sect = mommy.make(
+            BaseChoice, field_name='section', field_type=BaseChoice.MULTIPLE)
+        cs = mommy.make(ChoiceSection, base_ccobj=comp1, base_choice=sect)
+        choice = mommy.make(Choice, text='test_choice', choice_section=cs)
+        form = ReadyForm(**kwargs)
+        form.cleaned_data = {'form_name': 'testcc', 'section': [choice]}
+
+        self.assertFalse(CompletedCombinedObj.objects.all().exists())
+
+        form.save()
+
+        self.assertTrue(CompletedCombinedObj.objects.all().exists())
+        self.assertEqual(
+            CompletedCombinedObj.objects.get().form_data['section'][0],
+            choice.text)
